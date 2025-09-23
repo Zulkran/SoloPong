1e523372e71cde4055e71e8c2d7fcbebb04b1aff
Mary Ilhan <ilhan.mary@etu.unicaen.fr>
2025-09-23 14:57:22 +0200

    Changement majeur : utilisation de requestAnimationFrame() à la place de set Intervalle()

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ const rightButton = document.getElementById("right");
 // Player characteristics
 const paddle_thickness = 10;
 const paddle_width = 100;
-let XpaddlePosition = canvas.width / 2 - paddle_width / 2;
-const YpaddlePosition = canvas.height * (9/10);
+let XpaddlePosition = canvas.width / 2;
+const YpaddlePosition = canvas.height * (9.5/10);
+const paddle_speed = 10;
+
+// Déplacement
+let rightChanged = false;
+let leftChanged = false;
 
 // Ball dimension and position
 let x = canvas.width / 2;
@@ -18,35 +23,31 @@ let dx;
 let dy;
 const rayonBall = 10;
 
-// Time intervalle Game
-let intervalle;
-const intervalTime = 10;
+// Animation Game
+let animationGame;
 
 // Score
 let compteur = 0;
 let timeScore = 0;
 
-function drawBar() {
-    ctx.fillRect(XpaddlePosition, YpaddlePosition, paddle_width, paddle_thickness);
-}
-
-function drawBall() {
+function draw() {
+    // Draw Ball
     ctx.beginPath();
+
     ctx.arc(x, y, rayonBall, 0, Math.PI * 2, false);
     ctx.fillStyle = "white";
     ctx.fill();
-    ctx.closePath();
+
+    // Draw Bar 
+    ctx.fillRect(XpaddlePosition - paddle_width / 2, YpaddlePosition, paddle_width, paddle_thickness);
 }
 
-function changedScore() {
-    if (compteur == intervalTime*10) {
-        timeScore++;
-        scoreDisplay.textContent = "Score : " + timeScore + " s";
-        compteur = 0;
-    };
-}
+function update() {
+    // Changed bar coordonate
+    if(rightChanged == true && (XpaddlePosition + paddle_width / 2) < canvas.width) XpaddlePosition += paddle_speed;
+    if(leftChanged == true && (XpaddlePosition - paddle_width / 2) > 0) XpaddlePosition -= paddle_speed;
 
-function changedBall() {
+    // Changed Ball
     x += dx;
     y += dy;
 
@@ -54,39 +55,22 @@ function changedBall() {
     if(x + dx + rayonBall > canvas.width || x + dx - rayonBall < 0) {
         dx = -dx;
         dx > 0 ? dx+=0.1 : dx-=0.1;
-        drawBall();
     }
     else if (y + dy - rayonBall < 0) {
         dy = -dy;
         dy > 0 ? dy+=0.1 : dy-=0.1;
-        drawBall();
     }
+    // Finish Game
     else if (y + dy + rayonBall > canvas.height){
-        clearInterval(intervalle);
         resetGame();
     }
 
-    // Touch Bar
+    // Changed Touch Bar
     if(y >= YpaddlePosition && y <= YpaddlePosition + paddle_thickness) {
-        if(x >= XpaddlePosition && x <= XpaddlePosition + paddle_width){
+        if(x >= XpaddlePosition - paddle_width / 2 && x <= XpaddlePosition + paddle_width / 2){
             dy = -dy;
         }
     }
-
-
-}
-
-function changedBar(right, left) {
-    if((XpaddlePosition + paddle_width + right) > canvas.width || (XpaddlePosition - left) < 0) {
-        ;
-    }
-    else {
-        XpaddlePosition += right;
-        XpaddlePosition -= left;
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawBar();
 }
 
 function resetGame() {
@@ -100,82 +84,71 @@ function resetGame() {
     }
     dy= -2;
 
-    XpaddlePosition = canvas.width / 2 - paddle_width / 2;
+    XpaddlePosition = canvas.width / 2;
 
     compteur = 0;
     timeScore = 0;
+
+    cancelAnimationFrame(animationGame);
 }
+
 
 function game() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBar();
-    drawBall();
-    changedBall();
+    update();
+    draw();
 
-    compteur++;
-    changedScore();
-}
-
-function startGame() {
-    clearInterval(intervalle);
-    scoreDisplay.textContent = "Score : " + 0 + " s";
-    resetGame();
-    intervalle = setInterval(game, intervalTime);
+    animationGame = requestAnimationFrame(game);
 }
 
 newGameButton.addEventListener("click", () => {
-    startGame();
+    scoreDisplay.textContent = "Score : " + 0 + " s";
+    resetGame();
+    game();
 })
 
-let leftInterval;
-let rightInteval;
-leftButton.addEventListener("mousedown", () => {
-    leftInterval = setInterval(() => {
-        changedBar(0, 10);
-    }, 20);
-})
-rightButton.addEventListener("mousedown", () => {
-    rightInteval = setInterval(() => {
-        changedBar(10, 0);
-    }, 20);
-})
-leftButton.addEventListener("mouseup", () => {
-    clearInterval(leftInterval);
-})
+/* ####### Move controls ####### */
 
-rightButton.addEventListener("mouseup", () => {
-    clearInterval(rightInteval);
-})
-leftButton.addEventListener("mouseleave", () => {
-    clearInterval(leftInterval);
-})
+// Computer touch
+leftButton.addEventListener("mousedown", () => leftChanged = true);
+rightButton.addEventListener("mousedown", () => rightChanged = true);
 
-rightButton.addEventListener("mouseleave", () => {
-    clearInterval(rightInteval);
-})
+leftButton.addEventListener("mouseup", () => leftChanged = false);
+rightButton.addEventListener("mouseup", () => rightChanged = false);
+leftButton.addEventListener("mouseleave", () => leftChanged = false);
+rightButton.addEventListener("mouseleave", () => rightChanged = false);
 
+// Mobile touch
+leftButton.addEventListener("touchstart", () => leftChanged = true);
+rightButton.addEventListener("touchstart", () => rightChanged = true);
 
-// Event Mobile
-leftButton.addEventListener("touchstart", () => {
-    leftInterval = setInterval(() => {
-        changedBar(0, 10);
-    }, 20);
-})
-rightButton.addEventListener("touchstart", () => {
-    rightInteval = setInterval(() => {
-        changedBar(10, 0);
-    }, 20);
-})
+leftButton.addEventListener("touchend", () => leftChanged = false);
+rightButton.addEventListener("touchend", () => rightChanged = false);
 
-leftButton.addEventListener("touchend", () => {
-    clearInterval(leftInterval);
-})
-rightButton.addEventListener("touchend", () => {
-    clearInterval(rightInteval);
-})
+// Arrow Touch
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 37:
+        leftChanged = true;
+        break;
+      case 39:
+        rightChanged = true;
+        break;
+    }
+  };
+
+  document.onkeyup = function(e) {
+    switch (e.keyCode) {
+      case 37:
+        leftChanged = false;
+        break;
+      case 39:
+        rightChanged = false;
+        break;
+    }
+  };
 
 
 // Affichage de base
-drawBall();
-drawBar();
 resetGame();
+draw();
